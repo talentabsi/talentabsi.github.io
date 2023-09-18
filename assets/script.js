bc251182beb1e0231475b2d4b3458140f3dd8d05
diansigit <diansigit.p@gmail.com>
2023-09-18 22:20:35 +0700

    remove css clock

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 function clockUpdate() {
   var date = new Date();
-  $('.digital-clock').css({'color': '#fff', 'text-shadow': '0 0 6px #ff0'});
+  
   function addZero(x) {
     if (x < 10) {
       return x = '0' + x;
